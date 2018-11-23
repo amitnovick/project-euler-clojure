975be3329ef03aab3860ba3b0e525299a34f2453
feelextra <felxtra@gmail.com>
2018-11-23 13:31:03 +0200

    thanks for suggestion luma

--- 0001.clj
+++ 0001.clj
@@ -6,12 +6,10 @@
 (defn is-mod-five [n]
   (= (mod n 5) 0))
 
-(defn remove-bad-nums [numbers]
-  (defn is-good-num [n]
-    (or (is-mod-three n) (is-mod-five n)))
-  (filter is-good-num numbers))
+(defn is-good-num [n]
+  (or (is-mod-three n) (is-mod-five n)))
 
 (defn sum-good-nums [n]
-  (reduce + (remove-bad-nums (range 1 n))))
+  (reduce + (filter is-good-num (range 1 n))))
 
 (sum-good-nums 1000)
